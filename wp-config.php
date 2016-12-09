a396fc01d411fc9de51d157e0dffa62191e2c11b
Lindsey <lbohland@gmail.com>
2016-12-09 12:29:04 -0800

    dev wp-config

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'opps');
+define('DB_NAME', 'penton7_opps_dev');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'penton7_opps');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'dimmer');
+define('DB_PASSWORD', ']HLh\;Z4k)G)+!$(iR1e');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
@@ -77,7 +77,7 @@ $table_prefix  = 'wp_';
  *
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
-define('WP_DEBUG', true);
+define('WP_DEBUG', false);
 
 /* That's all, stop editing! Happy blogging. */
 
