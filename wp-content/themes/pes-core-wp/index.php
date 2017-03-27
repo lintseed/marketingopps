51423073ac01a0c83764b9dc2c42214ef969b759
lintseed <lbohland@gmail.com>
2017-03-27 11:19:28 -0600

    home page category8 list

--- wp-content/themes/pes-core-wp/index.php
+++ wp-content/themes/pes-core-wp/index.php
@@ -1,14 +1,13 @@
 <?php get_template_part('templates/page', 'header'); ?>
 
-<?php if (!have_posts()) : ?>
-  <div class="alert alert-warning">
-    <?php _e('Sorry, no results were found.', 'sage'); ?>
-  </div>
-  <?php get_search_form(); ?>
-<?php endif; ?>
-
-<?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
-<?php endwhile; ?>
-
-<?php the_posts_navigation(); ?>
+<p>Please select an event to browse opportunities:</p>
+<ul class="lead">
+   <?php
+     wp_list_categories( array(
+       'orderby'            => 'id',
+       'show_count'         => false,
+       'use_desc_for_title' => false,
+       'title_li'           => '',
+     ) );
+   ?>
+</ul>
